3cd2f101e501d5d5bc23b59bc35d0d19673d0c8e
barney-heatwave <djdise@outlook.com>
2020-11-01 16:11:31 +0000

    Fix issue #15

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
 // Here we use Moment.JS - it's outdated, but it works.
 moment();
 var now = moment().format("h.mma ddd D MMM");
-var time = document.getElementsByClassName(time).innerHTML = now;
+$('.time').html(now); // This line uses JQuery but that's ok since we have it in the HTML
