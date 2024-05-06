06edba77eb0214325f7e83f50c26e93c84949d4f
Sabian Roberts <31491602+sabianroberts@users.noreply.github.com>
2024-05-06 07:15:46 +0100

    Make d&t display dynamically update

--- www/date.js
+++ www/date.js
@@ -1,4 +1,8 @@
 // Here we use Moment.JS - it's outdated, but it works.
-moment();
-var now = moment().format("h.mma ddd D MMM");
-$('.time').html(now); // This line uses JQuery but that's ok since we have it in the HTML
+function updateClock() {
+    moment();
+    var now = moment().format("h.mma ddd D MMM");
+    $('.time').html(now); // This line uses JQuery but that's ok since we have it in the HTML
+}
+
+setInterval(updateClock, 1000);
